9c44913cd2c56c50163cb912c99cf7a8da83ed10
Herman <herman.orlov@ideo-digital.com>
2025-06-20 16:38:28 +0300

    renamed surveys folder to static surveys; added images and visualization plugin

---
+++ Survey.Web/App_Plugins/SurveyVisualizer/Js/survey-visualizer.controller.js
@@ -0,0 +1,9 @@
+﻿angular.module("umbraco").controller("SurveyVisualizer", function ($scope, editorState) {
+    var vm = this;
+
+    if (editorState.current.variants.find(item => item.name === "Measles Immunity Check")) {
+        vm.ImageToDisplay = "/App_Plugins/SurveyVisualizer/Images/MeaslesDiagram.svg";
+    } else {
+        vm.ImageToDisplay = "/App_Plugins/SurveyVisualizer/Images/ProfessionAdvisor.svg";
+    }
+});
